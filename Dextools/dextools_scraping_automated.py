5e24f3ceaa2508a61b6d4f5bf05199d72465303b
Jan Kubal <kubaljan@gmail.com>
2023-03-19 19:44:41 +0100

    First scraped batch in progress

--- Dextools/dextools_scraping_automated.py
+++ Dextools/dextools_scraping_automated.py
@@ -186,36 +186,47 @@ if __name__ == '__main__':
     pair_list = [
                 #("SHIB-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0x811beed0119b4afce20d2583eb608c6f7af1954f")#,
                 #("HEX-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0x55d5c232d921b9eaa6b37b5845e439acd04b4dba"),
-                ("AGIX-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0xe45b4a84e0ad24b8617a489d743c52b84b7acebe"),
-                # ("OPTIMUS-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0x8de7a9540e0edb617d78ca5a7c6cc18295fd8bb9"),
+                #("AGIX-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0xe45b4a84e0ad24b8617a489d743c52b84b7acebe"),
+                #("OPTIMUS-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0x8de7a9540e0edb617d78ca5a7c6cc18295fd8bb9"),
                 #("SHIK-WETH", "https://www.dextools.io/app/en/ether/pair-explorer/0x0b9f5cef1ee41f8cccaa8c3b4c922ab406c980cc"),
-                #("INJ-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x1bdcebca3b93af70b58c41272aea2231754b23ca"),
-                # ("VOLT-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x487bfe79c55ac32785c66774b597699e092d0cd9"),
-                # ("MBOX-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x8fa59693458289914db0097f5f366d771b7a7c3f"),
-                # ("FLOKI-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x231d9e7181e8479a8b40930961e93e7ed798542c"),
-                # ("BabyDoge-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0xc736ca3d9b1e90af4230bd8f9626528b3d4e0ee0")
+                ("INJ-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x1bdcebca3b93af70b58c41272aea2231754b23ca"),
+                ("VOLT-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x487bfe79c55ac32785c66774b597699e092d0cd9"),
+                ("MBOX-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x8fa59693458289914db0097f5f366d771b7a7c3f"),
+                ("FLOKI-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0x231d9e7181e8479a8b40930961e93e7ed798542c"),
+                ("BabyDoge-WBNB", "https://www.dextools.io/app/en/bnb/pair-explorer/0xc736ca3d9b1e90af4230bd8f9626528b3d4e0ee0")
                 ]
 
     #loop through the pairs to scrape
     for pair in pair_list:
 
-        data = scrape_data(pair)
+        attempts = 3
+        for i in range(attempts):
 
-        print(len(data))
-        for i in range(5):
-            print(data[i])
+            try:
+                data = scrape_data(pair)
 
-        # for i in data:
-        #     print(i)
+                print(len(data))
+                for i in range(5):
+                    print(data[i])
 
-        df = data_transform_save(data, pair)
-        print(df.iloc[:, 0:6].head(15))
-        print(df.info())
-        print(pair[0])
-        print(df.describe())
+                # for i in data:
+                #     print(i)
 
-        #df.to_csv("SHIB-WETH.csv", index=False)
-        save_dataframe_to_csv(df, pair[0])
+                df = data_transform_save(data, pair)
+                print(df.iloc[:, 0:6].head(15))
+                print(df.info())
+                print(pair[0])
+                print(df.describe())
+
+                #df.to_csv("SHIB-WETH.csv", index=False)
+                save_dataframe_to_csv(df, pair[0])
+                break
+
+            except Exception as e:
+                if i < attempts - 1:
+                    print(f"Attempt {i+1} failed with error: {e}")
+                else:
+                    raise Exception("Function failed after multiple attempts")
 
 
 
